e39f23017df2a4c3fef3a3ad8ad31bb352722821
brayans22 <bsaiago@fi.uba.ar>
2023-08-29 04:19:19 +0200

    ex18

--- Ex/ex18/ft_print_params.c
+++ Ex/ex18/ft_print_params.c
@@ -14,14 +14,14 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	j;
+	unsigned int	i;
+	unsigned int	j;
 
 	i = 1;
 	j = 0;
 	while (i < argc)
 	{
-		while (argv[i][j] != '\0')
+		while (argv[i][j])
 		{
 			write(1, &argv[i][j], 1);
 			j++;
